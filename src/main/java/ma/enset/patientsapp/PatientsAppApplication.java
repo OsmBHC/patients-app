6782612b22ae032efdbb3250b9b75848748a1768
BHC <oussamabhc@192.168.1.18>
2025-04-17 19:47:12 +0100

    Initial commit du projet Spring

---
+++ src/main/java/ma/enset/patientsapp/PatientsAppApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.patientsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/enset/patientsapp/PatientsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.patientsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
